2ca9ec14e16bbc56cfbd31c0ef936dafb1e072ea
preiramanda <101251381+preiramanda@users.noreply.github.com>
2025-06-16 14:24:11 +0200

    Update cleanup.sh

--- src/cleanup.sh
+++ src/cleanup.sh
@@ -1,19 +1,16 @@
 #!/bin/env bash
 
-cleanup::delete_merged_branches() {
-  local merged_prs=$1
-  for branch in $merged_prs; do
-    echo "Deleting merged branch: $branch"
-    github::delete_branch "$branch"
-  done
-}
-
 cleanup::delete_unmerged_branches() {
   local not_merged_prs=$1
+
   for branch in $not_merged_prs; do
+    if [[ " ${BASE_BRANCHES[*]} " == *" $branch "* ]]; then
+      echo "Branch protected or listed to be skipped: $branch"
+      continue
+    fi
+
     echo "Deleting not merged branch: $branch"
     github::delete_branch "$branch"
-
   done
 }
 
@@ -22,6 +19,11 @@ cleanup::delete_inactive_branches() {
   local inactive_branches=$(github::get_inactive_branches "$days_inactive")
 
   for branch in $inactive_branches; do
+    if [[ " ${BASE_BRANCHES[*]} " == *" $branch "* ]]; then
+      echo "Branch protected or listed to be skipped: $branch"
+      continue
+    fi
+
     echo "Deleting inactive branch: $branch"
     github::delete_branch "$branch"
   done
